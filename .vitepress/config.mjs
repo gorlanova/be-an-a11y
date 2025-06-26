bf2cef1df57e0739184f135031f68c4dcbe6cf99
Nina <nina.gautreau@gmail.com>
2025-06-26 14:39:57 +0200

    feat: head tags config

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -3,9 +3,32 @@ import { defineConfig } from "vitepress";
 
 export default defineConfig({
   title: "✨ Be an a11y",
-  description: "Demystifying the WCAG Guidelines",
+  description: "Demystifying the WCAG Guidelines - A guide to web accessibility",
 
-  head: [["link", { rel: "icon", type: "image/svg+xml", href: "/accessibility-light.svg" }]],
+  head: [
+    ["link", { rel: "icon", type: "image/svg+xml", href: "/accessibility-light.svg" }],
+    ["link", { rel: "icon", type: "image/svg+xml", href: "/accessibility-dark.svg", media: "(prefers-color-scheme: dark)" }],
+    ["link", { rel: "canonical", href: "https://be-an-a11y.netlify.app" }],
+    ["meta", { name: "color-scheme", content: "light dark" }],
+    ["meta", { name: "theme-color", content: "#ffffff" }],
+    ["meta", { name: "theme-color", content: "#000000", media: "(prefers-color-scheme: dark)" }],
+    ["meta", { name: "author", content: "Nina Gautreau" }],
+    ["meta", { name: "keywords", content: "accessibility, a11y, wcag, web development, web design" }],
+    ["meta", { name: "application-name", content: "Be an A11y" }],
+    // og tags
+    ["meta", { property: "og:title", content: "✨ Be an a11y" }],
+    ["meta", { property: "og:description", content: "Demystifying the WCAG Guidelines - A guide to web accessibility" }],
+    ["meta", { property: "og:image", content: "https://be-an-a11y.netlify.app/card.png" }],
+    ["meta", { property: "og:image:alt", content: "Web Accessibility logo" }],
+    ["meta", { property: "og:url", content: "https://be-an-a11y.netlify.app" }],
+    ["meta", { property: "og:type", content: "website" }],
+    // twitter tags
+    ["meta", { name: "twitter:card", content: "summary_large_image" }],
+    ["meta", { name: "twitter:title", content: "✨ Be an a11y" }],
+    ["meta", { name: "twitter:description", content: "Demystifying the WCAG Guidelines - A guide to web accessibility" }],
+    ["meta", { name: "twitter:image", content: "https://be-an-a11y.netlify.app/card.png" }],
+    ["meta", { name: "twitter:image:alt", content: "Web Accessibility logo" }],
+  ],
 
   themeConfig: {
     nav: [
